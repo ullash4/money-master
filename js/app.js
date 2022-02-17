312f69406231f0fc9f1734fe0165a3a5b4182778
ullash4 <programmerullash@gmail.com>
2022-02-17 15:35:37 +0600

    commented

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Make calculate button functional 
 document.getElementById('calculate-btn').addEventListener('click', function (){
     const food = document.getElementById('food-input').value;
     const foodTk = parseFloat(food);
@@ -5,39 +6,32 @@ document.getElementById('calculate-btn').addEventListener('click', function (){
     const rentTk = parseFloat(rent);
     const bills = document.getElementById('bills-input').value;
     const billsTk = parseFloat(bills);
-
+    // Error handling 
     if(  food == '' || food < 0 || rent == '' || rent < 0 || bills == '' || bills < 0 ){
         document.getElementById('fail-sms').style.display ='block';
     }
     else{
-    let totalCost = 0;
-    totalCost = foodTk + rentTk + billsTk;
-    const expenses = incomeCalculation() - totalCost;
+        let totalCost = 0;
+        totalCost = foodTk + rentTk + billsTk;
+        const expenses = incomeCalculation() - totalCost;
 
-    const totalExpenses = document.getElementById('expenses-tag');
-    totalExpenses.innerText = totalCost;
-    const mainBalance = document.getElementById('balance-tag');
-    mainBalance.innerText = expenses;
-    document.getElementById('fail-sms').style.display ='none';
-
+        totalExpenses.innerText = totalCost;
+        const totalExpenses = document.getElementById('expenses-tag');
+        mainBalance.innerText = expenses;
+        const mainBalance = document.getElementById('balance-tag');
+        
+        document.getElementById('fail-sms').style.display ='none';
     }
-
-    /* let totalCost = 0;
-    totalCost = foodTk + rentTk + billsTk;
-    const expenses = incomeCalculation() - totalCost;
-
-    const totalExpenses = document.getElementById('expenses-tag');
-    totalExpenses.innerText = totalCost;
-    const mainBalance = document.getElementById('balance-tag');
-    mainBalance.innerText = expenses; */
 });
 
+// Function to get Income Balance 
 function incomeCalculation(){
     let income = document.getElementById('income-input').value;
     let incomeTk = parseFloat(income);
     return incomeTk;
 }
 
+// Make save button functional 
 document.getElementById('save-btn').addEventListener('click', function (){
     const saving = document.getElementById('save-input').value;
     const savingTk = parseFloat(saving);
@@ -50,10 +44,13 @@ document.getElementById('save-btn').addEventListener('click', function (){
     const balanceInTk = parseFloat(mainBalance.innerText);
     let remain = 0;
     remain = balanceInTk - mySaving;
-    
+    // Error handling 
+    if(mySaving > remain || mySaving < 0){
+        document.getElementById('fail-save-sms').style.display ='block';
+    }
+    else{
         const remaining = document.getElementById('remain-tag');
         remaining.innerText = remain;
-    /* const remaining = document.getElementById('remain-tag');
-    remaining.innerText = remain;
- */
+        document.getElementById('fail-save-sms').style.display ='none';
+    }
 })
