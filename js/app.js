818abff7c2537ffe3447d45fe40e68d17967dea0
ullash4 <programmerullash@gmail.com>
2022-02-17 12:00:56 +0600

    4th commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 document.getElementById('calculate-btn').addEventListener('click', function (){
-    // const income = document.getElementById('income-input').value;
-    // const incomeTk = parseFloat(income);
     const food = document.getElementById('food-input').value;
     const foodTk = parseFloat(food);
     const rent = document.getElementById('rent-input').value;
@@ -8,6 +6,10 @@ document.getElementById('calculate-btn').addEventListener('click', function (){
     const bills = document.getElementById('bills-input').value;
     const billsTk = parseFloat(bills);
 
+    if(  food == '' || food < 0 || rent == '' || rent < 0 || bills == '' || bills < 0 ){
+        document.getElementById('fail-sms').style.display ='block';
+    }
+    else{
     let totalCost = 0;
     totalCost = foodTk + rentTk + billsTk;
     const expenses = incomeCalculation() - totalCost;
@@ -16,7 +18,20 @@ document.getElementById('calculate-btn').addEventListener('click', function (){
     totalExpenses.innerText = totalCost;
     const mainBalance = document.getElementById('balance-tag');
     mainBalance.innerText = expenses;
+    document.getElementById('fail-sms').style.display ='none';
+
+    }
+
+    /* let totalCost = 0;
+    totalCost = foodTk + rentTk + billsTk;
+    const expenses = incomeCalculation() - totalCost;
+
+    const totalExpenses = document.getElementById('expenses-tag');
+    totalExpenses.innerText = totalCost;
+    const mainBalance = document.getElementById('balance-tag');
+    mainBalance.innerText = expenses; */
 });
+
 function incomeCalculation(){
     let income = document.getElementById('income-input').value;
     let incomeTk = parseFloat(income);
@@ -24,8 +39,6 @@ function incomeCalculation(){
 }
 
 document.getElementById('save-btn').addEventListener('click', function (){
-    // let income = document.getElementById('income-input').value;
-    // let incomeTk = parseFloat(income);
     const saving = document.getElementById('save-input').value;
     const savingTk = parseFloat(saving);
     let mySaving= 0;
@@ -37,7 +50,10 @@ document.getElementById('save-btn').addEventListener('click', function (){
     const balanceInTk = parseFloat(mainBalance.innerText);
     let remain = 0;
     remain = balanceInTk - mySaving;
-    const remaining = document.getElementById('remain-tag');
+    
+        const remaining = document.getElementById('remain-tag');
+        remaining.innerText = remain;
+    /* const remaining = document.getElementById('remain-tag');
     remaining.innerText = remain;
-
+ */
 })
